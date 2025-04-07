659972c262ab2ad0126a6da882b92d87c0c7a507
Sujoy Das <sujoydaspc2023@gmail.com>
2025-04-07 23:58:46 +0600

    upload index.js

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-const app = require(express);
-
+const app = express()
 const port = process.env.PORT || 5000
 
+app.use(cors())
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+res.send("Simple-crud-server is running")
+})
+
+app.listen(port,()=>{
+    console.log(`simple crud is running on port: ${port}`)
+})
