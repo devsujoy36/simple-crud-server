3e6e4c20de80b2222a3a77b1d4653e61d66c687d
devsujoydas <devsujoydas@gmail.com>
2025-05-23 13:14:22 +0600

    finish

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+require("dotenv").config();
 
 const app = express()
 const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
-
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-const uri = "mongodb+srv://devsujoydas:mongodbdevsujoydas@cluster0.f1vo05q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// const uri = "mongodb://localhost:27017";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f1vo05q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -25,12 +26,6 @@ async function run() {
     const userModel = client.db("usersDB").collection("users");
 
 
-    app.post("/adduser", async (req, res) => {
-      const user = req.body;
-      const result = await userModel.insertOne(user);
-      res.send(result)
-    })
-
     app.get("/users", async (req, res) => {
       const result = await userModel.find().toArray()
       res.send(result)
@@ -43,30 +38,28 @@ async function run() {
       res.send(user)
     })
 
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await userModel.insertOne(user);
+      res.send(result)
+    })
+
+    app.put("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const { name, username, email, password } = req.body;
+      const query = { _id: new ObjectId(id) }
+      const options = { upsert: true }
+      const updatedUser = { $set: { name, username, email, password } }
+      const result = await userModel.updateOne(query, updatedUser, options)
+      res.send(result)
+    })
+
     app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const result = await userModel.deleteOne(query)
       res.send(result)
     })
-
-    app.put("/user/:id", async (req, res) => {
-      const id = req.params.id;
-      const user = req.body;
-      const query = { _id: new ObjectId(id) }
-      const options = { upsert: true }
-      const updatedUser = {
-        $set: {
-          name: user.name,
-          username: user.username,
-          email: user.email,
-          password: user.password,
-        }
-      }
-      const result = await userModel.updateOne(query, updatedUser, options)
-      res.send(result)
-    })
-
 
 
     await client.db("admin").command({ ping: 1 });
